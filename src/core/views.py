cd4407e9900f3a573ee5941b6ee82d12cba25a9d
leandro <leandro1979@hotmail.com>
2024-12-13 15:32:04 -0300

    Creando URLs y Views

--- src/core/views.py
+++ src/core/views.py
@@ -2,3 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def saludar(request):
+    return HttpResponse('Lo de Leandrito')
+
+def saludar_con_etiqueta(request):
+    return HttpResponse('<h1> La mejor Musica Trascendental</h1>')
+
+def saludar_con_parametros(request, nombre: str, apellido:str):
+    nombre = nombre.capitalize()
+    apellido = apellido.capitalize()
+    return HttpResponse(f'{apellido}, {nombre}')
